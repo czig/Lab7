adc9c05326758aecbb8055c6c1c19b89787145c9
Caleb Ziegler <C15Caleb.Ziegler@usafa.edu>
2013-12-07 12:27:05 -0700

    Commented code (did a thorough job commenting code so I could better understand it and it will make the library easier to use). Also, I changed the getLeftSensor() conditional statement to only turn on the LED at closer distances.

--- main.c
+++ main.c
@@ -4,31 +4,33 @@
 /*
  * main.c
  */
+
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
     initSensors();
-	P1DIR |= BIT0;
-	P1DIR |= BIT6;
+	P1DIR |= BIT0;  // Set P1.0 as output for left LED
+	P1DIR |= BIT6;  // Set P1.6 as output for right LED
     while (1)
     {
-
-    	if (getLeftSensor() < 0x2FE)
+    	// Turn on left LED on if after ADC, left sensor returns a value greater than 2FE, and if not, turn LED off
+    	if (getLeftSensor() < 0x3CC)
     	{
-    		P1OUT &= ~BIT0;
+    		P1OUT &= ~BIT0; // turn left LED off
     	}
     	else
     	{
-    		P1OUT |= BIT0;
+    		P1OUT |= BIT0; // turn left LED on
     	}
 
+    	// Turn on right LED if after ADC, right sensor returns a value greater than 1FF, and if not, turn LED off
     	if (getRightSensor() < 0x1FF)
     	{
-    		P1OUT &= ~BIT6;
+    		P1OUT &= ~BIT6; // turn right LED off
     	}
     	else
     	{
-    		P1OUT |= BIT6;
+    		P1OUT |= BIT6; // turn right LED on
     	}
 
     }
--- sensor.c
+++ sensor.c
@@ -4,36 +4,37 @@
  * Description: Implementation file for
  * operating sensor on robot.
  ***********************************/
+
 #include <msp430.h>
 #include "sensor.h"
 
 void initSensors(void)
 {
-	ADC10CTL0 &= ~ENC;
-	ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
-	ADC10AE0 |= BIT3|BIT4|BIT5;                         // PA.1 ADC option select
-	ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
-	ADC10CTL1 |= BIT7|BIT6|BIT5;
+	ADC10CTL0 &= ~ENC;                          // Disable the core to make changes to control bits in ADC
+	ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // Select longest sampling period (sample-and-hold time), turn on subsystem, and enable interrupts
+	ADC10AE0 |= BIT3|BIT4|BIT5;                 // Set P1.3, P1.4, and P1.5 (A3, A4, A5) as analog input for ADC
+	ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;         // Select SMCLK to be ADC clock source
+	ADC10CTL1 |= BIT7|BIT6|BIT5;                // Divide SMCLK by 8 to ensure slowest possible clock time to prevent problems due to loading (makes ADC more accurate at close distances)
 }
 
 unsigned int getLeftSensor()
 {
-	 ADC10CTL0 &= ~ENC;
-	 ADC10CTL1 &= ~(INCH_15);
-	 ADC10CTL1 |= INCH_3;                       // input A4
-	 ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
-	 __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
+	 ADC10CTL0 &= ~ENC;                         // Disable the core to make changes to control bits in ADC
+	 ADC10CTL1 &= ~(INCH_15);                   // Clear input channel
+	 ADC10CTL1 |= INCH_3;                       // Set input channel to A3
+	 ADC10CTL0 |= ENC + ADC10SC;                // Enable the core and begin sampling/conversion
+	 __bis_SR_register(CPUOFF + GIE);           // Go into low power mode and enable interrupts
 
 	 return ADC10MEM;
 }
 
 unsigned int getRightSensor()
 {
-	ADC10CTL0 &= ~ENC;
-	ADC10CTL1 &= ~(INCH_15);
-	ADC10CTL1 |= INCH_4;                       // input A4
-	ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
-	__bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
+	ADC10CTL0 &= ~ENC;                          // Disable the core to make changes to control bits in ADC
+	ADC10CTL1 &= ~(INCH_15);					// Clear input channel
+	ADC10CTL1 |= INCH_4;                        // Set input channel to A4
+	ADC10CTL0 |= ENC + ADC10SC;                 // Enable the core and begin sampling/conversion
+	__bis_SR_register(CPUOFF + GIE);            // Go into low power mode and enable interrupts
 
 	return ADC10MEM;
 
@@ -41,11 +42,11 @@ unsigned int getRightSensor()
 
 unsigned int getCenterSensor()
 {
-	ADC10CTL0 &= ~ENC;
-	ADC10CTL1 &= ~(INCH_15);
-	ADC10CTL1 |= INCH_5;                       // input A4
-	ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
-	__bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
+	ADC10CTL0 &= ~ENC;      					// Disable core to make changes to control bits in ADC
+	ADC10CTL1 &= ~(INCH_15);					// Clear input channel
+	ADC10CTL1 |= INCH_5;                        // Set input channel to A5
+	ADC10CTL0 |= ENC + ADC10SC;                 // Enable the core and begin sampling/conversion
+	__bis_SR_register(CPUOFF + GIE);            // Go into low power mode and enable interrupts
 
 	return ADC10MEM;
 
@@ -55,5 +56,5 @@ unsigned int getCenterSensor()
 #pragma vector=ADC10_VECTOR
 __interrupt void ADC10_ISR(void)
 {
-  __bic_SR_register_on_exit(CPUOFF);        // Clear CPUOFF bit from 0(SR)
+  __bic_SR_register_on_exit(CPUOFF);            // Re-enable CPU and execute next instructions after low power mode was set
 }
